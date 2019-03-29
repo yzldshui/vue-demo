8ed3a5450a7619c77f2bca0c7bc4a8ae55cbd1d0
luoxy111 <982549067@qq.com>
2019-03-29 17:55:23 +0800

    做作业

--- src/views/homework/input.vue
+++ src/views/homework/input.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <el-form>
-      <el-form-item label="我的任务">
-        <el-input v-model="newItem" placeholder="做点啥好呢？"/>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="createItem(newItem)">创建任务</el-button>
-        <el-button @click="emptyItem">清空输入</el-button>
-      </el-form-item>
+    <el-form style="margin-top: 20px;">
+      <el-col :span="16">
+        <el-form-item >
+          <el-input v-model="newItem" placeholder="再做点啥好呢？"/>
+        </el-form-item>
+      </el-col>
+      <el-col :span="8">
+        <el-form-item style="float:right">
+          <el-button type="primary" @click="createItem(newItem)">创建</el-button>
+          <el-button @click="emptyItem">清空输入</el-button>
+        </el-form-item>
+      </el-col>
     </el-form>
   </div>
 </template>
--- src/views/homework/todolist.vue
+++ src/views/homework/todolist.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="app-container">
+    <el-row class="todo-txt">
+      <el-col :span="8">
+        <el-card class="item-all" shadow="hover">
+          当前任务总数：{{ data.length }}
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card class="item-undone" shadow="hover">
+          未完成任务数：{{ countUndone }}
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card class="item-doing" shadow="hover">
+          进行中任务数：{{ countDoing }}
+        </el-card>
+      </el-col>
+    </el-row>
     <todo-input @create="createTask"/>
     <el-table
       v-loading="listLoading"
       :data="data"
       element-loading-text="Loading"
+      height="500"
       border
       fit>
+      <el-table-column
+        :index="index"
+        type="index"/>
       <el-table-column prop="content" label="任务" min.width="500"/>
       <el-table-column class-name="status-col" label="状态" width="200" align="center">
         <template slot-scope="scope">
@@ -21,9 +42,14 @@
         </template>
       </el-table-column>
     </el-table>
-    <div class="todo-txt">
-      当前任务总数：{{ data.length }}
-    </div>
+    <el-form style="margin-top: 20px;">
+      <el-col :span="2" style="font-size: 14px;color: gray;text-align: center;">
+        任务完成总进度：
+      </el-col>
+      <el-col :span="22">
+        <el-progress :text-inside="true" :stroke-width="18" :percentage="countDonePer"/>
+      </el-col>
+    </el-form>
   </div>
 
 </template>
@@ -65,26 +91,41 @@ export default {
       newTask: '',
       data: [
         {
-          content: '扶老奶奶过马路',
-          status: 0
+          content: '扶老奶奶过马路！',
+          status: 2
         },
         {
-          content: '把钱交给警察叔叔',
+          content: '把钱交给警察叔叔！',
           status: 1
         },
         {
-          content: '吃饭吃饭吃饭吃饭吃饭吃饭',
-          status: 0
-        },
-        {
-          content: '睡觉睡觉睡觉睡觉睡觉',
-          status: 0
-        },
-        {
-          content: '工作工作工作工作工作工作',
+          content: '石室诗士施氏，嗜狮，誓食十狮。',
           status: 2
+        },
+        {
+          content: '睡觉睡觉睡觉睡觉睡觉！',
+          status: 0
+        },
+        {
+          content: '西溪犀，喜嬉戏。席熙夕夕携犀徙，席熙细细习洗犀。',
+          status: 0
+        },
+        {
+          content: '工作工作工作工作工作工作工作工作工作工作！',
+          status: 1
         }
       ]
+    }
+  },
+  computed: {
+    countUndone() {
+      return this.data.filter(item => item.status < 2).length
+    },
+    countDoing() {
+      return this.data.filter(item => item.status === 1).length
+    },
+    countDonePer() {
+      return Number((((1 - this.countUndone / this.data.length) * 100).toFixed(2)))
     }
   },
   beforeCreate() {
@@ -97,10 +138,44 @@ export default {
   },
   methods: {
     deleteRow(index, rows) {
-      rows.splice(index, 1)
+      this.$confirm('此操作将永久删除该任务, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+        rows.splice(index, 1)
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消操作'
+        })
+      })
     },
     updateStatus(index) {
-      this.data[index].status = (this.data[index].status + 1) % 3
+      if (this.data[index].status < 2) {
+        this.data[index].status = (this.data[index].status + 1) % 3
+      } else {
+        this.$confirm('此操作将重新打开已完成的任务, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '重新打开成功!'
+          })
+          this.data[index].status = (this.data[index].status + 1) % 3
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消操作'
+          })
+        })
+      }
     },
     getItemClass(status) {
       if (status === 0) {
@@ -117,6 +192,9 @@ export default {
       if (v) {
         this.data.push({ content: v, status: 0 })
       }
+    },
+    index(index) {
+      return index
     }
   }
 }
@@ -135,8 +213,19 @@ export default {
     color: green;
   }
   .todo-txt {
-    color: #409eff;
     font-size: 14px;
-    margin: 20px 0px 0px 12px;
+    margin: 0px -10px 0px -10px;
+    .item-all{
+      color: #409eff;
+      margin: 0px 10px;
+    }
+    .item-undone{
+      color: gray;
+      margin: 0px 10px;
+    }
+    .item-doing{
+      color: orangered;
+      margin: 0px 10px;
+    }
   }
 </style>
